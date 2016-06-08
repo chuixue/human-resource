ad0deb146ef823a8d442537b9b05005e88cd4d4d
chuixue <simonblowsnow@163.com>
2016-06-08 21:23:45 +0800

    back up before the fesval of duanwu

--- js/head.js
+++ js/head.js
@@ -1,15 +1,26 @@
 // JavaScript Document
-var path = window.document.location.href,
-	phead = path;
+/*
 
-alert(path);
+var root = "human-resource";
+var href = window.document.location.href, run_type = href.substr(0, 4), 
+	ls = href.split(root);
+//if("file" == run_type);
 
+if(ls.length == 0)alert("Error: The web folder changed, so change the value of 'root' paramter in file js/head.js " + 
+		"with the name of parent folder of 'index.html'");
+var path = ls[ls.length-1].split("/");
+path.shift();
+path.pop();
+var tm_path = "";
+path.forEach(function(){ tm_path += "../"; });
+*/
+var tm_path = "";
 
 $.ajaxSetup({ cache:false });
 $.ajaxSetup({ async: false });  
 var IsReady=0;
-$("<div></div>").load("../head.html",function(){
+$("<div></div>").load(tm_path + "head.html",function(){
 	document.write(this.innerHTML.match(/<xmp>([\s\S]*?)<\/xmp>/)[1]);
 	IsReady=1;
 });
-$j.ajaxSetup({ async: true });
+$.ajaxSetup({ async: true });
--- js/tail.js
+++ js/tail.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $.ajaxSetup({ async: false });  
-$("<div></div>").load("../tail.html",function(){
+$("<div></div>").load(tm_path + "tail.html",function(){
 	document.write(this.innerHTML.match(/<xmp>([\s\S]*?)<\/xmp>/)[1]);
 	IsReady=2;
 });
