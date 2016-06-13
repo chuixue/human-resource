6808fe076833365a2a8c1a46c4b2f06c2cb41854
chuixue <simonblowsnow@163.com>
2016-06-13 23:50:38 +0800

    use new include way.

---
+++ bin/head.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+/*
+
+var root = "human-resource";
+var href = window.document.location.href, run_type = href.substr(0, 4), 
+	ls = href.split(root);
+//if("file" == run_type);
+
+if(ls.length == 0)alert("Error: The web folder changed, so change the value of 'root' paramter in file js/head.js " + 
+		"with the name of parent folder of 'index.html'");
+var path = ls[ls.length-1].split("/");
+path.shift();
+path.pop();
+var tm_path = "";
+path.forEach(function(){ tm_path += "../"; });
+*/
+var tm_path = "";
+
+$.ajaxSetup({ cache:false });
+$.ajaxSetup({ async: false });  
+var IsReady=0;
+$("<div></div>").load(tm_path + "index-home2.html",function(){
+	document.write(this.innerHTML.match(/<xmp>([\s\S]*?)<\/xmp>/)[1]);
+	IsReady=1;
+	
+});
+$.ajaxSetup({ async: true });
+
+ $(document).ready(function() { 
+  
+  });
+	$(window).resize(function(){ setFrame(); });
+	function setFrame(){ $("#fm-main").height(1000 - 158); };
